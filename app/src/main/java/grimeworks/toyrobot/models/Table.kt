18d549bdb684dc6f4ecf3e64d52f478a3df3ae97
Lauren Grimes <lauren.grimes@gmail.com>
2017-12-06 21:15:23 +1100

    Added basic readme.md

--- app/src/main/java/grimeworks/toyrobot/models/Table.kt
+++ app/src/main/java/grimeworks/toyrobot/models/Table.kt
@@ -6,13 +6,13 @@ import android.graphics.Point
  * Created by laurengrimes on 6/12/17.
  */
 
-class Table internal constructor(private val width: Int, private val height: Int) {
-    private val origin: Point? = null
+class Table(private val width: Int, private val height: Int) {
+    private var origin: Point? = null
 
     fun setOrigin(origin: Point) {
         // Origin requested is outside allowable range
-        if ((0..width).contains(origin.x) == false || (0..height).contains(origin.y) == false) {
-
+        if ((0..width).contains(origin.x) && (0..height).contains(origin.y)) {
+            this.origin = origin
         }
     }
 }
